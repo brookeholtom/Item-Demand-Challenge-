0d24a37d1e44b3ccc1e34362f3a671ba2bf427d2
brookeholtom <130940524+brookeholtom@users.noreply.github.com>
2023-11-15 14:01:11 -0700

    11/15

--- Store Item Demand/ItemDemand.R
+++ Store Item Demand/ItemDemand.R
@@ -13,8 +13,8 @@ library(themis)
 
 
 
-train <- read.csv("C:/Users/brook/Downloads/STAT348/Store Item Demand/train.csv")
-test <- read.csv("C:/Users/brook/Downloads/STAT348/Store Item Demand/test.csv")
+train <- vroom("train.csv")
+test <-  vroom("test.csv")
 
 plot1 <- train %>%
   filter(store==1, item==1) %>%
@@ -38,3 +38,75 @@ plot4 <- train %>%
 
 
 wrap_plots(plot1, plot2, plot3, plot4)
+
+
+storeItem <- train %>%
+filter(store==7, item==17) 
+
+#Random Forest 
+my_recipe <- recipe(sales ~., data=storeItem) %>%
+step_date(date, features="doy") %>%
+step_date(date, features="dow") #%>%
+#step_range(date_doy, min=0, max=pi) %>%
+#step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) 
+
+prep <- prep(my_recipe)
+bake(prep, new_data = storeItem)
+
+my_mod_forest <- rand_forest(mtry = tune(),
+                             min_n=tune(),
+                             trees=500) %>%
+  set_engine("ranger") %>%
+  set_mode("regression")
+
+## Create a workflow with model & recipe
+workflow_forest <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod_forest)
+
+
+## Set up grid of tuning values
+tuning_grid <- grid_regular(mtry(range=c(1, ncol(storeItem) - 1)),
+                            min_n(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+
+
+## Set up K-fold CV
+folds <- vfold_cv(storeItem, v = 10, repeats=1)
+
+
+CV_results_forest <- workflow_forest %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(smape))
+
+
+## Find best tuning parameters
+bestTune <- CV_results_forest %>%
+  select_best("smape")
+
+collect_metrics(CV_results_forest) 
+
+## Finalize workflow and predict
+
+final_wf_forest <-
+  workflow_forest %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=storeItem)
+
+
+predictions_forest <- final_wf_forest %>%
+  predict(test, type = "prob")
+
+predictions_forest <- predictions_forest %>%
+  bind_cols(., test) %>%
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
+
+vroom_write(x= predictions_forest, file="predictions_forest_final.csv", delim=",")
+
+
+
+
+
